3f7a318caeb8ecf2264297265c130e9a30126052
chc0331 <ss01003@naver.com>
2025-11-11 23:28:23 +0900

    Semantics

--- app/src/main/java/com/example/composetoglance/MainActivity.kt
+++ app/src/main/java/com/example/composetoglance/MainActivity.kt
@@ -12,14 +12,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetoglance.ui.theme.ComposeToGlanceTheme
@@ -37,6 +44,9 @@ class MainActivity : ComponentActivity() {
                     .fillMaxSize()
                     .background(Color.Blue)
                     .provideLogger(logger)
+                    .semantics {
+                        this[SemanticsPropertyKey(name = "parent")] = 0
+                    }
                     .track("Box1")
             ) {
 
@@ -46,7 +56,14 @@ class MainActivity : ComponentActivity() {
                         .track("Column1")
                         .provideLogger(logger)
                 ) {
-                    Text(text = "Test", modifier = Modifier.track("Text1"))
+                    var show by remember { mutableStateOf(false) }
+                    Button(onClick = {
+                        show = !show
+                    }) {
+                        Text("Button", modifier = Modifier.track("ButtonText"))
+                    }
+                    if (show)
+                        Text(text = "Test", modifier = Modifier.track("ShowText"))
                 }
             }
         }
@@ -54,9 +71,5 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-//        Log.i("heec.choi","Tree : $collector")
     }
 }
-
-private val id = AtomicInteger(1)
-fun generateUniqueId() = id.incrementAndGet()
