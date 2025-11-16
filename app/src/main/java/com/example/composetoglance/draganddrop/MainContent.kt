7770f0e016aaf66a02855ba7239188f1df273a64
chc0331 <ss01003@naver.com>
2025-11-16 12:03:35 +0900

    TopBar 추가.

--- app/src/main/java/com/example/composetoglance/draganddrop/MainContent.kt
+++ app/src/main/java/com/example/composetoglance/draganddrop/MainContent.kt
@@ -3,6 +3,12 @@ package com.example.composetoglance.draganddrop
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -10,11 +16,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.composetoglance.draganddrop.bottompanel.BottomPanelWithTabs
 import com.example.composetoglance.draganddrop.canvas.WidgetCanvas
 import com.example.composetoglance.draganddrop.layout.Layout
 import com.example.composetoglance.draganddrop.widget.Widget
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContent() {
     val widgets = remember {
@@ -25,20 +33,31 @@ fun MainContent() {
     }
     var selectedLayout by remember { mutableStateOf<Layout?>(null) }
 
-    LongPressDrawable(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            WidgetCanvas(
-                selectedLayout = selectedLayout,
-                modifier = Modifier
-                    .weight(3f)
-                    .fillMaxWidth()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Glance") },
+                modifier = Modifier.height(64.dp)
             )
+        }
+    ) { paddingValues ->
+        LongPressDrawable(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                WidgetCanvas(
+                    selectedLayout = selectedLayout,
+                    modifier = Modifier
+                        .weight(2.8f)
+                        .fillMaxWidth()
+                )
 
-            BottomPanelWithTabs(
-                widgets = widgets,
-                onLayoutSelected = { selectedLayout = it },
-                modifier = Modifier.weight(1f)
-            )
+                BottomPanelWithTabs(
+                    widgets = widgets,
+                    onLayoutSelected = { selectedLayout = it },
+                    modifier = Modifier.weight(1.2f)
+                )
+            }
         }
     }
 }
