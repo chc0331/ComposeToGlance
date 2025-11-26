8c4f74dab2209aa87fcdbed3c39ed1082b4ea91e
chc0331 <ss01003@naver.com>
2025-11-26 23:20:59 +0900

    scale factor - 1.5f -> 1.2f

--- app/src/main/java/com/example/composetoglance/editor/draganddrop/DragAndDropConstants.kt
+++ app/src/main/java/com/example/composetoglance/editor/draganddrop/DragAndDropConstants.kt
@@ -4,7 +4,7 @@ package com.example.composetoglance.editor.draganddrop
  * 드래그 앤 드롭 관련 상수
  */
 internal object DragAndDropConstants {
-    const val SCALE_FACTOR = 1.5f
+    const val SCALE_FACTOR = 1.2f
     const val DRAG_ALPHA = 0.9f
 }
 
