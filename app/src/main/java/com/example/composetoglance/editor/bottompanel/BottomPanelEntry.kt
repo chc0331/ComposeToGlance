0b2a8b8add249d16589e4cd3622d9a24687538ac
chc0331 <ss01003@naver.com>
2025-11-26 23:24:50 +0900

    TabRow에 커스텀 Indicator 구현

--- app/src/main/java/com/example/composetoglance/editor/bottompanel/BottomPanelEntry.kt
+++ app/src/main/java/com/example/composetoglance/editor/bottompanel/BottomPanelEntry.kt
@@ -1,6 +1,11 @@
 package com.example.composetoglance.editor.bottompanel
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -12,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +41,8 @@ object BottomPanelConstants {
     val LAYOUT_ITEM_SPACING = 8.dp
     val WIDGET_LIST_PADDING = 16.dp
     val WIDGET_LIST_SPACING = 8.dp
+    val TAB_INDICATOR_WIDTH = 80.dp // Tab Indicator의 너비 (짧게 설정)
+    val TAB_INDICATOR_HEIGHT = 3.dp // Tab Indicator의 높이
 }
 
 @Composable
@@ -54,10 +62,20 @@ fun BottomPanelWithTabs(
             containerColor = MaterialTheme.colorScheme.surface,
             contentColor = MaterialTheme.colorScheme.onSurface,
             indicator = { tabPositions ->
-                TabRowDefaults.SecondaryIndicator(
-                    modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
-                    color = MaterialTheme.colorScheme.secondary
-                )
+                Box(
+                    modifier = Modifier
+                        .tabIndicatorOffset(tabPositions[tabIndex])
+                        .fillMaxHeight()
+                        .width(BottomPanelConstants.TAB_INDICATOR_WIDTH),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(BottomPanelConstants.TAB_INDICATOR_WIDTH)
+                            .height(BottomPanelConstants.TAB_INDICATOR_HEIGHT)
+                            .background(MaterialTheme.colorScheme.secondary)
+                    )
+                }
             }
         ) {
             tabs.forEachIndexed { index, title ->
