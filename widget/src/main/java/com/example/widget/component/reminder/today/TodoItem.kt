f23cc7c88c2bcb66527bd919049c0cc15244dc82
chc0331 <ss01003@naver.com>
2025-12-17 22:15:52 +0900

    파일 이름 변경

--- widget/src/main/java/com/example/widget/component/reminder/today/TodoItem.kt
+++ widget/src/main/java/com/example/widget/component/reminder/today/TodoItem.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.outlined.CheckCircle
