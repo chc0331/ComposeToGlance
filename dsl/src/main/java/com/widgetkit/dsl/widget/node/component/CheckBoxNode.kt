212315bbdca9a4c3c64c3d7d8bc97f3e07d491f7
chc0331 <ss01003@naver.com>
2025-12-22 13:14:42 +0900

    GlanceCheckBox 구현

--- dsl/src/main/java/com/widgetkit/dsl/widget/node/component/CheckBoxNode.kt
+++ dsl/src/main/java/com/widgetkit/dsl/widget/node/component/CheckBoxNode.kt
@@ -6,7 +6,7 @@ import com.widgetkit.dsl.proto.WidgetType
 import com.widgetkit.dsl.widget.WidgetRenderer
 import com.widgetkit.dsl.widget.node.RenderContext
 import com.widgetkit.dsl.widget.node.RenderNode
-import com.widgetkit.dsl.widget.render.glance.render.GlanceButton
+import com.widgetkit.dsl.widget.render.glance.render.GlanceCheckBox
 
 internal class CheckBoxNode : RenderNode {
     @Composable
@@ -16,7 +16,7 @@ internal class CheckBoxNode : RenderNode {
         renderer: WidgetRenderer
     ) {
         if (node.widgetType == WidgetType.WIDGET_TYPE_GLANCE) {
-            GlanceButton.render(node, context, renderer)
+            GlanceCheckBox.render(node, context, renderer)
         } else {
 
         }
